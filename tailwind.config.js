ef1d6a833a86f68e4d8a8c6706f0edda7ac60272
AssoumaRoumanatou <assoumaroumanatou08@gmail.com>
2024-08-05 04:04:41 +0100

    Mon projet web

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "1.5xl": "1408px",
+      "2xl": "1536px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
